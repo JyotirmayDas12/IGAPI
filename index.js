557a72e4811fc792fdc97e6bbada4118515412a4
JyotirmayDas12 <98383961+JyotirmayDas12@users.noreply.github.com>
2024-04-10 13:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', async (req, res) => {
 
         res.json(logData);
     } catch (error) {
-        res.status(500).json({ error: message: "Send Valid Instagram URL. join: "@INDIAN_DEV",
+        res.status(500).json({ error: "message: "Send Valid Instagram URL." join: "@INDIAN_DEV",
             credit: "Made by JYOTIRMAY DAS" " });
     }
 });
